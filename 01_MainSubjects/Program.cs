ebbf6ca4b15cee56ee0002b245bd93755ae45d7c
Nisaİhtiyar <nisa.ihtiyar@bil.omu.edu.tr>
2024-10-22 13:47:43 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region YazdirmaKomutlari
+
+            //Console.Write("Hello World!");
+            //Console.WriteLine("Selam!");
+            //Console.WriteLine("Asagidayim");
+
+            //Console.WriteLine("*****Yemek Kategorileri*****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Corbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soguk Baslangiclar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlilar");
+            //Console.WriteLine("6-Icecekler");
+
+            #endregion
+
+            #region Degiskenler
+
+            char sifre = 'a'; // tek tirnak ile belirtilir
+
+            #endregion
+
+            #region Klavyeden "String" veri girisleri
+
+            //String passengerName, passengerSurname, passengerIdentityNumber;
+
+            //Console.Write("Yolcu Adi: ");
+            //passengerName = Console.ReadLine();
+
+            //Console.Write("Yolcu Soyadi: ");
+            //passengerSurname = Console.ReadLine();
+
+            //Console.Write("Yolcu TC Kimlik Numarasi: ");
+            //passengerIdentityNumber = Console.ReadLine();
+
+            //Console.WriteLine();
+            //Console.WriteLine("---Yolcu Bilgileri---");
+            //Console.WriteLine("Yolcu : "+ passengerName+ " "+ passengerSurname );
+            //Console.WriteLine("Tc Kimlik Numarasi : "+ passengerIdentityNumber );
+
+            #endregion
+
+            #region Klavyeden "Int" veri girisleri
+
+            //int sayi1;
+
+            //Console.Write("Sayi giriniz: ");
+            //sayi1 = int.Parse(Console.ReadLine()); // int.Parse ile stringe donustururuz
+
+            ////double icin double.Parse
+            ////char icin char.Parse kullanilabilir
+
+
+            //Console.WriteLine("Girdiginiz sayi : " + sayi1);
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
+
+
+
